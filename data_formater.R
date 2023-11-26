8b4142b62d0c86b7db42845e957c887f80b5da8c
Philipp Schickel <P.Schickel@campus.lmu.de>
2023-11-26 13:45:46 +0100

    tweaked a little and added Daten_only_Bodyparts

--- data_formater.R
+++ data_formater.R
@@ -1,6 +1,6 @@
 library(tidyverse)
-library(stringr)
-#formats all hourly data into long format
+Daten <- read_csv("Daten.csv")
+# this code chunk formats all hourly data into long format
 
 
 Daten_longer <- Daten %>% rename("AT 20" = "A 20","AT 22" = "A 22") %>% rename("Notizen" = ...163)%>% 
@@ -11,4 +11,8 @@ Daten_longer <- Daten %>% rename("AT 20" = "A 20","AT 22" = "A 22") %>% rename("
     names_to = c("bodypart", "hour"), # new column names, ATTENTION: bodypart contains things like clothing etc.
     names_pattern = "(.*\\D)(\\d{1,2})$", # pattern to separate the original column names
     values_to = "temperatur" # new column name for the values
-  )
+  ) %>% mutate(hour = as.numeric(hour)) %>%
+  mutate(bodypart = gsub(" ","",bodypart)) # remove spaces in bodypart
+
+all_bodyparts <- c("TDl", "TDr", "KT", "TPl", "TPR", "TF", "TG", "TN", "TZ")
+Daten_longer_only_bodypart <- Daten_longer %>% filter(bodypart == all_bodyparts) # only bodyparts
